5be41fc7748e255e8a0760105e99ce92f3aaadd1
jtri <jtriphahn@repayonline.com>
2016-07-12 09:44:47 -0700

    updated appveyor and tentacle deploy script

--- DeployOctopusTentacle.ps1
+++ DeployOctopusTentacle.ps1
@@ -13,6 +13,8 @@ ForEach ($Module in $Modules) {
     $Source = Join-Path -Path $ScriptDir -ChildPath $Module
     Copy-Item -Path $Source -Destination $($ModulePath) -ErrorAction SilentlyContinue
 }
+#$wmf5 = https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12-KB3134759-x64.msu
+#iwr $wmf5 -outfile 'C:\wmf5.msu'
 
 # Hardcode config file for now
 $ConfigFile = Join-Path -Path $ScriptDir -ChildPath TentacleConfig.ps1
@@ -22,5 +24,7 @@ $ConfigData = Join-Path -Path $ScriptDir -ChildPath TentacleData.psd1
 .\VariableSubstitution.ps1 -Files $ConfigData -SubPattern $SubPattern
 
 # Compile DSC Configuration
+.\TentacleConfig.ps1 -ConfigData $ConfigData
 
 # Start DSC Configuration
+Start-DscConfiguration .\TentacleConfig -Wait -Verbose -Force
